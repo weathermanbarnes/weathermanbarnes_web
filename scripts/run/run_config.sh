570d31aa848e9dc3aa0f6fb233a6035ec416bf59
Michael Barnes <39979861+weathermanbarnes@users.noreply.github.com>
2023-05-16 16:39:51 +1000

    Update run_config.sh

--- scripts/run/run_config.sh
+++ scripts/run/run_config.sh
@@ -1,5 +1,8 @@
 export HOME_DIR="${HOME_DIR:-/home/565/mb0427}"
 export GDATA_DIR="${GDATA_DIR:-/g/data/w40/mb0427}"
-export MONASHWEB_DIR="${GDATA_DIR}"/"MonashWeb"
 
-export CONDA_MODULE_PATH="${CONDA_BASE}"/./"${MODULE_SUBDIR}"/"${MODULE_NAME}"
+export MONASHWEB_DIR="${GDATA_DIR}"/"MonashWeb"
+export FORECASTS_DIR="${MONASHWEB_DIR}"/"forecasts"
+export DATA_DIR="${MONASHWEB_DIR}"/"data"
+
+
