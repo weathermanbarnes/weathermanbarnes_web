c87c57db405ca9f784b9021e0bdd7e7677126136
Michael Barnes <mb0427@gadi-dm-01.gadi.nci.org.au>
2023-06-08 13:23:19 +1000

    Updated files from GADI

---
+++ .ipynb_checkpoints/submit-checkpoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+export DATE=$(date +%Y%m%d -d "1 day ago")
+export RUN=12
+
+###### Load conda modules ######
+module use /g/data/hh5/public/modules
+module load conda/analysis3
+
+###### Change to the repo directory ######
+cd /home/565/mb0427/MonashWeb/monash_weather_web/
+
+###### Run python scripts ######\n\
+rm -rf data
+python3 scripts/GFS/plot_GFS.py ${DATE} ${RUN}
+python3 plot_copy_to_dir.py
+
+sleep 0.5
