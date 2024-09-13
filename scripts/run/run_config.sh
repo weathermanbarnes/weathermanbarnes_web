2d0b919c7170d8dc551acff3c39921c4b9a18c5d
Michael Barnes <39979861+weathermanbarnes@users.noreply.github.com>
2024-09-13 15:02:30 +1000

    Delete scripts directory

--- scripts/run/run_config.sh
+++
@@ -1,8 +0,0 @@
-export HOME_DIR="${HOME_DIR:-/home/565/mb0427}"
-export GDATA_DIR="${GDATA_DIR:-/g/data/w40/mb0427}"
-
-export MONASHWEB_DIR="${GDATA_DIR}"/"MonashWeb"
-export FORECASTS_DIR="${MONASHWEB_DIR}"/"forecasts"
-export DATA_DIR="${MONASHWEB_DIR}"/"data"
-
-
